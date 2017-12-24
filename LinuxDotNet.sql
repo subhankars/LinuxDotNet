7e4cc0150db5eca64db79d01d39e89c25fbdfc13
Subhankar Sarkar <s.subhankar@gmail.com>
2017-12-24 11:38:49 +0530

    Initial Commit - a Simple Cross Platform ASP.NET Core MVC Web application with Entity Framework Core connected to Sql Server on Linux

---
+++ LinuxDotNet.sql
@@ -0,0 +1,23 @@
+﻿-- Create database
+IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'LinuxDotNet')
+    BEGIN
+        CREATE DATABASE LinuxDotNet
+END;
+-- Use the created database to create tables
+USE [LinuxDotNet]
+IF  NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[dbo].[Customer]') AND type in (N'U'))
+BEGIN	
+	CREATE TABLE Customer (
+    ID int identity primary key, 
+    FirstName varchar(50),
+    LastName varchar(50),
+    Email varchar(100),
+    Age   int
+);
+
+END
+
+-- Insert Sample data
+TRUNCATE TABLE Customer
+Insert INTO Customer Values('John', 'Doe', 'johndoe@email.com',45),('Mike', 'Penn', 'mikepen@email.com',34),('Walter', 'Mar', 'walter@email.com',32),('Suzan', 'J', 'suzi@email.com',56),('Kyle', 'Day', 'kday@email.com',27);
---
+++ Models/LinuxDotNetContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace LinuxDotnet.Models
+{
+    public partial class LinuxDotNetContext : DbContext
+    {
+        public static string ConnectionString { get; set; }
+        public virtual DbSet<Customer> Customer { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var result = modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
